6eba79be444b5a9c37c6f5e0963dda97d6ff3aa3
Jacqueline Sabino <sabino.jacke@hotmail.com>
2018-09-12 07:08:22 -0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        string caminho = @"c:\temp\";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool DiretoryExist()
+        {
+            
+            if (Directory.Exists(caminho))
+                return true;
+
+            return false;
+        }
+
+        private FileInfo[] GetImages()
+        {
+            DirectoryInfo directorysearch = new DirectoryInfo(caminho);
+            FileInfo[] images = directorysearch.GetFiles();
+            return images;
+        }
+
+        private void StoreImageFifo()
+        {
+            Queue fifoImages = new Queue();
+            FileInfo[] collectionImages = GetImages(); 
+            foreach (var fileImage in collectionImages)
+            {
+                fifoImages.Enqueue(fileImage);
+            }
+        }
+
+        
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            if (DiretoryExist())
+            {
+                StoreImageFifo();
+
+            }
+        }
+    }
+}
